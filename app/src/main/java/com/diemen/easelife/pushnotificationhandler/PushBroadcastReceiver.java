c0017e337c6905a15c2d515be5145c5191192152
AnujKS <anuj301091@gmail.com>
2015-01-15 18:15:38 +0530

    chat Adapter new changes

--- app/src/main/java/com/diemen/easelife/pushnotificationhandler/PushBroadcastReceiver.java
+++ app/src/main/java/com/diemen/easelife/pushnotificationhandler/PushBroadcastReceiver.java
@@ -15,7 +15,7 @@ public class PushBroadcastReceiver extends ParsePushBroadcastReceiver{
     @Override
     public void onPushOpen(Context context, Intent intent) {
         Log.e("Push", "Clicked");
-        Intent i = new Intent(context, StartActivity.class);
+        Intent i = new Intent(context, ChatActivity.class);
         i.putExtras(intent.getExtras());
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
