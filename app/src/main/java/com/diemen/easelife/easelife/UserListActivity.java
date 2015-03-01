f3805454a1338911b05824581c7de6544377424d
AnujKS <anuj301091@gmail.com>
2015-03-01 11:28:30 +0530

    userlist MessGE
    MESSAGE

--- app/src/main/java/com/diemen/easelife/easelife/UserListActivity.java
+++ app/src/main/java/com/diemen/easelife/easelife/UserListActivity.java
@@ -3,17 +3,25 @@ package com.diemen.easelife.easelife;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.diemen.easelife.model.Chat;
 import com.diemen.easelife.model.Subcategory;
 import com.diemen.easelife.model.User;
 import com.diemen.easelife.pushnotificationhandler.ChatActivity;
 import com.diemen.easelife.sqllite.DBManager;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -36,6 +44,8 @@ public class UserListActivity extends ActionBarActivity {
         list.setAdapter(adapter);
         list.setItemsCanFocus(true);
 
+
+
         // Click event for single list row
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -51,6 +61,20 @@ public class UserListActivity extends ActionBarActivity {
                 gotoChat.putExtra("SenderPhone",currentUser.getString("PhoneNumber"));
                 gotoChat.putExtra("Sender",currentUser.getUsername());
                 gotoChat.putExtra("SrcActivity","UserListActivity");
+
+
+
+
+                Chat chat = new Chat();
+                chat.setMessage(" ");
+                chat.setSelf(true);
+                chat.setSender(currentUser.getUsername());
+                chat.setSenderPhone(currentUser.getString("PhoneNumber"));
+                chat.setReceiver(user.getName());
+                chat.setReceiverPhone(user.getPhoneNo());
+                SendMessage(chat);
+
+
                 startActivity(gotoChat);
                 finish();
             }
@@ -65,6 +89,30 @@ public class UserListActivity extends ActionBarActivity {
             mList.add(u);
         }
         mList.add(new User("Anuj", "234234234242"));
+
+    }
+
+    public void SendMessage(Chat chat)
+    {
+        try {
+            Date date = new Date();
+            DBManager.getInstance().addChat(chat);
+            JSONObject data;
+            ParseQuery pQuery = new ParseInstallation().getQuery();
+            pQuery.whereEqualTo("phone", chat.getReceiverPhone());
+            ParsePush pushMessage = new ParsePush();
+            pushMessage.setQuery(pQuery);
+            //Always keep in mind "I am the Sender"
+            //when the user will receive this message he will be the sender;
+            data = new JSONObject("{\"alert\":\"New Message\",\"Message\": \"" + chat.getMessage() + "\",\"ReceiverPhone\": \"" + chat.getSenderPhone() + "\",\"Sender\":\"" + chat.getReceiver() + "\",\"Receiver\":\"" + chat.getSender() + "\" ,\"SenderPhone\":\"" + chat.getReceiverPhone() + "\",\"ReceivedOn\":\"" + date.getTime() + "\"}");
+            pushMessage.setMessage("New Message");
+            pushMessage.setData(data);
+            pushMessage.sendInBackground();
+        }
+        catch (Exception e)
+        {
+            Log.e(" UserList Activity"," Method:Send Message");
+        }
 
     }
 
